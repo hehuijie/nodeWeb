d8cdf8cd24a60dbd72d6e4f234c3f85af83057db
hehuijie <m18810146471@163.com>
2018-03-14 17:35:06 +0800

    nodeWeb
    node+express

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/reg', function(req, res, next) {
+  res.render('reg', { title: '注册' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: '登录' });
+});
+
 module.exports = router;
